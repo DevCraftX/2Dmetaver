73a48c13b36992f0c1c0d6918c66515bc5c09bcc
adesh_ghadage <adeshghadage9@gmail.com>
2024-10-26 23:14:42 +0530

    update two avtar

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ const sizes = {
   height: window.innerHeight,
 };
 
-
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game");
     this.player;
+    this.remotePlayer;
     this.cursor;
     this.playerSpeed = 100;
     this.socket = null;
-    this.playerId = "player"+ Math.random()*10; // Assign a unique player ID for now
+    this.playerId = "player1"; // Assign a unique player ID
   }
 
   // load the assets
@@ -31,7 +31,7 @@ class GameScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor("#84AFA6");
     const table = this.physics.add.staticImage(this.cameras.main.width / 2, this.cameras.main.height / 2, "table").setOrigin(0.5, 0.5);
-    
+
     this.anims.create({
       key: 'walk',
       frames: [
@@ -50,25 +50,36 @@ class GameScene extends Phaser.Scene {
       repeat: -1
     });
 
+    // Create player and remote player sprites
     this.player = this.physics.add.sprite(400, 300, "avatar").setScale(0.1);
-    this.player.setImmovable(false);
-    this.player.body.allowGravity = false;
-    this.player.setCollideWorldBounds(true);
-    this.physics.add.collider(this.player, table);
+    this.remotePlayer = this.physics.add.sprite(500, 300, "avatar").setScale(0.1); // initial position for remote player
+    this.remotePlayer.setTint(0x00ff00); // Different color to distinguish remote player
 
+    this.player.setCollideWorldBounds(true);
+    this.remotePlayer.setCollideWorldBounds(true);
     this.cursor = this.input.keyboard.createCursorKeys();
 
     // Initialize the WebSocket connection
     this.socket = new WebSocket("ws://10.145.107.131:8080/ws");
 
-    this.socket.onopen = (e) => {
+    this.socket.onopen = () => {
       console.log("[open] Connection established");
       this.socket.send(`Player connected: ${this.playerId}`);
     };
 
     this.socket.onmessage = (event) => {
-      console.log(`[message] Data received from server: ${event.data}`);
+      const data = JSON.parse(event.data);
+      if (data.playerId !== this.playerId) {
+        this.updateRemotePlayer(data);
+      }
     };
+  }
+
+  // Update the position of the remote player based on received data
+  updateRemotePlayer(data) {
+    if (this.remotePlayer) {
+      this.remotePlayer.setPosition(data.x, data.y);
+    }
   }
 
   // The game loop, Runs every frame to manage game logic
